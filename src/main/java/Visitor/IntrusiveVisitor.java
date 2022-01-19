02c858f39e83ff9a7552989cdd897956fe3204a4
Chinmay Sharma <chinmaysharmacs10@gmail.com>
2022-01-19 16:55:45 +0530

    Added visitor files

---
+++ src/main/java/Visitor/IntrusiveVisitor.java
@@ -0,0 +1,53 @@
+package Visitor;
+
+abstract class Expression {
+    public abstract void print(StringBuilder stringBuilder);
+}
+
+class DoubleExpression extends Expression {
+    public double value;
+
+    public DoubleExpression(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public void print(StringBuilder stringBuilder) {
+        stringBuilder.append(value);
+    }
+}
+
+class AdditionExpression extends Expression {
+    public Expression left, right;
+
+    public AdditionExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public void print(StringBuilder stringBuilder) {
+        stringBuilder.append('(');
+        left.print(stringBuilder);
+        stringBuilder.append('+');
+        right.print(stringBuilder);
+        stringBuilder.append(')');
+    }
+}
+
+// Intrusive visitor violates SIP and OCP
+public class IntrusiveVisitor {
+    public static void main(String[] args) {
+        AdditionExpression expression = new AdditionExpression(
+                new DoubleExpression(1),
+                new AdditionExpression(
+                        new DoubleExpression(2),
+                        new DoubleExpression(3)
+                )
+        );
+
+        StringBuilder stringBuilder = new StringBuilder();
+        expression.print(stringBuilder);
+        System.out.println(stringBuilder);
+    }
+}
